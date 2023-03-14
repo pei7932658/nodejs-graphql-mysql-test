ec6084f328c14a4862413ad2987625960c384c84
peiliang <peiliang@megvii.com>
2023-03-14 10:52:46 +0800

    fix bug

--- src/schema/mutations.js
+++ src/schema/mutations.js
@@ -1,10 +1,11 @@
 const { GraphQLObjectType } = require('graphql')
 const baconMutation = require('../model/bacon/mutations')
+const visitMutation = require('../model/visits/mutations')
 
 module.exports = new GraphQLObjectType({
     name: 'RootMutationsType',
     fields: {
-        addBacon: baconMutation.addBacon,
-        updateBacon: baconMutation.updateBacon
+        addVisit: visitMutation.addVisit,
+        updateVisit: visitMutation.updateVisit
     }
 })
--- src/schema/queries.js
+++ src/schema/queries.js
@@ -1,10 +1,13 @@
 const { GraphQLObjectType } = require('graphql')
 const baconQueries = require('../model/bacon/queries')
+const visitQueries = require('../model/visits/queries')
 
 module.exports = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         bacon: baconQueries.bacon,
-        bacons: baconQueries.bacons
+        bacons: baconQueries.bacons,
+        visit: visitQueries.visit,
+        visits: visitQueries.visits
     }
 })
